3b5f0e6945fbb158eb170bd968451429ccc9cf8d
Ramon <ramondsf92@gmail.com>
2019-01-23 19:21:59 -0200

    Adicionado try/except para opção

--- tierlistfgo.py
+++ tierlistfgo.py
@@ -10,49 +10,52 @@ print('''Digite a opção desejada:
 3 - Abrir team comp.
 Digite qualquer outra tecla pra sair.''')
 
-op = (input())
+try:
+    op = int(input())
+    if op == 1: #Servo
+        servo = input('Digite o nome do servo para consulta: ').lower().strip()
+        servo = servo.replace(' ', '-')
+        if '(' in servo:
+            servo = servo.replace('(', '')
+            servo = servo.replace(')', '')
 
-if op == '1': #Servo
-    servo = input('Digite o nome do servo para consulta: ').lower().strip()
-    servo = servo.replace(' ', '-')
-    if '(' in servo:
-        servo = servo.replace('(', '')
-        servo = servo.replace(')', '')
+        print(servo)
 
-    print(servo)
+        webbrowser.open_new_tab('https://grandorder.gamepress.gg/servant/' + servo)
 
-    webbrowser.open_new_tab('https://grandorder.gamepress.gg/servant/' + servo)
-
-elif op == '2': #Tier list
-    print('''1 - Abrir tier list de 5 estrelas.
+    elif op == 2: #Tier list
+        print('''1 - Abrir tier list de 5 estrelas.
 2 - Abrir tier list de 4 estrelas.
 3 - Abrir tier list de 1-3 estrelas
 Digite qualquer outra tecla pra sair.''')
 
-    op = input()
-    if op == '1':
-        webbrowser.open_new(url + '5-star-tier-list/')
-    elif op == '2':
-        webbrowser.open_new_tab(url + '4-star-tier-list/')
-    elif op == '3':
-        webbrowser.open_new_tab(url + '13-star-tier-list/')
+        op = input()
+        if op == '1':
+            webbrowser.open_new(url + '5-star-tier-list/')
+        elif op == '2':
+            webbrowser.open_new_tab(url + '4-star-tier-list/')
+        elif op == '3':
+            webbrowser.open_new_tab(url + '13-star-tier-list/')
 
-elif op == '3': #Team comp
-    print('''1 - Abrir Comp Buster Blitz
+    elif op == 3: #Team comp
+        print('''1 - Abrir Comp Buster Blitz
 2 - Abrir Comp Buster Critfest
 3 - Abrir Arts Spamfest
 4 - Abrir Zombie Team
 5 - Abrir Staple Quick Team
 Digite qualquer outra tecla pra sair.''')
 
-    op = input()
-    if op == '1':
-        webbrowser.open_new_tab(url + 'buster-blitz/')
-    elif op == '2':
-        webbrowser.open_new_tab(url + 'buster-critfest/')
-    elif op == '3':
-        webbrowser.open_new_tab(url + 'arts-spamfest/')
-    elif op == '4':
-        webbrowser.open_new_tab(url + 'zombie-team/')
-    elif op == '5':
-        webbrowser.open_new_tab(url + 'staple-quick-team/')
+        op = int(input())
+        if op == 1:
+            webbrowser.open_new_tab(url + 'buster-blitz/')
+        elif op == 2:
+            webbrowser.open_new_tab(url + 'buster-critfest/')
+        elif op == 3:
+            webbrowser.open_new_tab(url + 'arts-spamfest/')
+        elif op == 4:
+            webbrowser.open_new_tab(url + 'zombie-team/')
+        elif op == 5:
+            webbrowser.open_new_tab(url + 'staple-quick-team/')
+except ValueError:
+    print("Invalid input.")
+
